fb493525f210b865a7a79b6955ebfc7ddf32cc0f
Unknown <38399178+Awware@users.noreply.github.com>
2020-03-31 17:22:11 +0600

    TestUpdate

--- ClientLogic/ClientLogic.cs
+++ ClientLogic/ClientLogic.cs
@@ -11,7 +11,7 @@ namespace ClientLogic
         public ClientLogic()
         {
             ClientEvents.Debug?.Invoke("DEBUG");
-            ClientEvents.Info?.Invoke("INFO");
+            ClientEvents.Info?.Invoke("INFO2");
             ClientEvents.Warn?.Invoke("WARN");
             ClientEvents.Error?.Invoke("ERROR");
             ClientEvents.Success?.Invoke("SUCCESS");
