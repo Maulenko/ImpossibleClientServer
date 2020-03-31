717ecd46c8e0d585c3e27c7d680c5ce7ce387ffe
Maulenko <Maulenko@users.noreply.github.com>
2020-03-31 17:25:04 +0600

    Revert "zz"
    This reverts commit 80a7f4d65814384d2a98939330f86e66d8bdc26d.

--- ClientLogic/ClientLogic.cs
+++ ClientLogic/ClientLogic.cs
@@ -11,7 +11,7 @@ namespace ClientLogic
         public ClientLogic()
         {
             ClientEvents.Debug?.Invoke("DEBUG");
-            ClientEvents.Info?.Invoke("INFOqwe");
+            ClientEvents.Info?.Invoke("INFO");
             ClientEvents.Warn?.Invoke("WARN");
             ClientEvents.Error?.Invoke("ERROR");
             ClientEvents.Success?.Invoke("SUCCESS");
